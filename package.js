d12e2f338436fb7b452ff45fe5469d992ff4707d
Jan Dvorak <storytellercz@proton.me>
2024-06-18 10:38:20 +0200

    Update mizzao:timesync

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'mizzao:user-status',
   summary: 'User connection and idle state tracking for Meteor',
-  version: '2.0.0-beta.0',
+  version: '2.0.0-beta.1',
   git: 'https://github.com/Meteor-Community-Packages/meteor-user-status.git'
 });
 
@@ -16,7 +16,7 @@ Package.onUse((api) => {
   api.use('mongo');
   api.use('ddp');
   api.use('tracker', 'client');
-  api.use('mizzao:timesync@0.5.4');
+  api.use('mizzao:timesync@0.5.5');
 
   api.export('MonitorInternals', 'client', {
     testOnly: true
@@ -33,7 +33,7 @@ Package.onUse((api) => {
 Package.onTest((api) => {
   api.use('ecmascript');
   api.use('mizzao:user-status');
-  api.use('mizzao:timesync@0.5.4');
+  api.use('mizzao:timesync@0.5.5');
 
   api.use(['accounts-base', 'accounts-password']);
 
